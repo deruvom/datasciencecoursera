9032962fe45c2989b9c783befc9b52ff60281d7a
deruvom <micol.deruvo@gmail.com>
2017-07-31 19:36:20 +0200

    Linear Regression Basics

--- LM.R
+++ LM.R
@@ -2,9 +2,6 @@
 ## Linear Regression basics 
 
 
-model <- lm(y ~ 1)     ## Intercept only (no slope coefficient)
-model <- lm(y ~ x - 1) ## Slope only (no intercept coefficient)
+model <- lm(y ~ 1)     ## Intercept only (no slope coefficient -x)
+model <- lm(y ~ x - 1) ## Slope only (no intercept coefficient - subtract 1)
 model <- lm(y ~ x)     ## Intercept & slope (x is not a factor)
-model <- lm(y ~ w)     ## Intercepts only (w is a factor)
-model <- lm(y ~ x + w) ## Intercepts & slope against confounding x & w
-model <- lm(y ~ x * w) ## Intercepts & slopes against interacting x & w
